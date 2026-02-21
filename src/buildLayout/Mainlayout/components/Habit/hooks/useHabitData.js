ff06b7286875374b25c7a527292744648fda6434
prajwallakra05 <prajwallakra05@gmail.com>
2026-02-21 11:24:16 +0530

    fixed deployment

--- src/buildLayout/Mainlayout/components/Habit/hooks/useHabitData.js
+++ src/buildLayout/Mainlayout/components/Habit/hooks/useHabitData.js
@@ -1,5 +1,5 @@
 import { useMemo } from "react"
-import { useApp } from "../../../../context/AppContext"
+import { useApp } from "../../../../../context/AppContext"
 
 export function useHabitData() {
   const { habits, tasks } = useApp()
